66f5cf0dd8e5200b7047ea8283241f2a7d704852
unknown <teona.piranishvili1@gmail.com>
2023-04-21 15:06:38 +0400

    chore

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://live-chat-front-two.vercel.app/",
+    origin: "http://live-chat-front-two.vercel.app",
   },
 });
 io.on("connection", (socket) => {
