8d507b3954feb794b92463709293aae01c73b01d
unknown <teona.piranishvili1@gmail.com>
2023-04-21 14:50:51 +0400

    feat: live chat

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 import express from "express";
 import http from "http";
 import cors from "cors";
+import { Server } from "socket.io";
 const port = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
 
-app.get("/api", (req, res) => {
-  res.send("Hellow fron other world");
+const io = new Server(server, {
+  cors: {
+    origin: "http://127.0.0.1:5173",
+  },
 });
-server.listen(port, () => {
-  console.log("server running");
+io.on("connection", (socket) => {
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+  socket.on("disconnect", () => {});
 });
+
+server.listen(port);
