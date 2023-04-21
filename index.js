1b396c390d29d0a97bb90d2a0e371bfc8fa14bc3
unknown <teona.piranishvili1@gmail.com>
2023-04-21 12:51:58 +0400

    setup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import http from "http";
 import cors from "cors";
-
+const port = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
@@ -9,6 +9,6 @@ const server = http.createServer(app);
 app.get("/api", (req, res) => {
   res.send("Hellow fron other world");
 });
-server.listen(3001 || process.env.PORT, () => {
+server.listen(port, () => {
   console.log("server running");
 });
